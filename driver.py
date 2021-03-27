f727b5f658f8cffd24922560ab368c7b2732d04e
0xfinlay <finlay.richardson1@gmail.com>
2021-03-27 21:16:23 +0000

    Update script

--- driver.py
+++ driver.py
@@ -3,6 +3,8 @@ import requests
 from uuid import uuid4
 from urllib.parse import quote
 
+from requests.api import head
+
 
 class Firefly:
     def __init__(self, host, appID="Firefly.py Driver"):
@@ -20,7 +22,6 @@ class Firefly:
         response = ET.fromstring(response.content)
         if not eval(response.attrib['exists'].title()):
             raise Exception("Invalid response")
-
         host = response[1].text
         ssl = eval(response[1].attrib['ssl'].title())
         if ssl:
@@ -53,12 +54,45 @@ class Firefly:
         return id
 
     def get_auth_url(self):
-        if not self.deviceID:
+        if not hasattr(self, "deviceID"):
             self.set_device_id()
         redirect = quote(
             f"{self.host}/Login/api/gettoken?ffauth_device_id={self.deviceID}&ffauth_secret=&device_id={self.deviceID}&app_id={self.appID}")
         return f"{self.host}/login/login.aspx?prelogin={redirect}"
 
+    def complete_auth(self, xml):
+        token = ET.fromstring(xml)
+        self.secret = token[0].text
+        self.user = {'username': token[1].attrib['username'],
+                     'fullname': token[1].attrib['fullname'],
+                     'email': token[1].attrib['email'],
+                     'role': token[1].attrib['role'],
+                     'guid': token[1].attrib['guid']}
+        self.classes = []
+        for lesson in token[1][0]:
+            self.classes.append({'guid': lesson.attrib['guid'],
+                                 'name': lesson.attrib['name'],
+                                 'subject': lesson.attrib['subject']})
 
-school = Firefly.get_host("ESMS")
-print(school)
+    def graph_query(self, query):
+        data = quote(query)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        return requests.post(f"{self.host}/_api/1.0/graphql?ffauth_device_id={self.deviceID}&ffauth_secret={self.secret}", data=data, headers=headers)
+
+    def get_configuration(self):
+        response = self.graph_query("""query Query {
+				configuration {
+					week_start_day, weekend_days, native_app_capabilities, notice_group_guid 
+				}
+			}""")  # Fix this
+        print(response.text)
+
+
+school = Firefly("https://esms.fireflycloud.net")
+
+school.set_device_id()
+print(school.get_auth_url())
+input()
+xml = open("auth.xml", "r").read()
+school.complete_auth(xml)
+school.get_configuration()
